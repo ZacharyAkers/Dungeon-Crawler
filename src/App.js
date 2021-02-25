08f2b555ca4c9908dd85a4c746043f4757bb0a59
ZacharyAkers <zakhariakers@gmail.com>
2021-02-25 14:11:10 -0500

    bug fix

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 
-import DownloadAppPopup       from './components/download-app-popup';
 import DialogManager          from './features/dialog-manager';
 import EndlessFloorCounter    from './components/endless-floor-counter';
 import Footer                 from './components/footer';
