fb35964c21e2f0d5d462550f5060e4aad95c14f6
ZacharyAkers <zakhariakers@gmail.com>
2021-02-25 14:12:51 -0500

    removed file

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react';
 
 import DialogManager          from './features/dialog-manager';
 import EndlessFloorCounter    from './components/endless-floor-counter';
-import Footer                 from './components/footer';
 import GameMenus              from './features/game-menus';
 import World                  from './features/world';
 import Viewport               from './components/viewport';
